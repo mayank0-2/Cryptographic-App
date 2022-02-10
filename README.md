95f9b30df92e9f9e31fef7bdbc26143c6063bea8
Mayank kumar <mayank02091999@gmail.com>
2022-02-10 10:53:30 +0530

    Moved to View Binding and added modified swipe buttons

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cryptographic-App
+
+            Useful Links used to understand working of app's internals
+
+> swipebutton:- https://github.com/stfalcon-studio/swipeable-button
+> viewBinding:- https://developer.android.com/topic/libraries/view-binding
+> Handler:- https://developer.android.com/reference/android/os/Handler
--- app/src/main/java/com/example/cryptograhic_app/MainActivity.kt
+++ app/src/main/java/com/example/cryptograhic_app/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.cryptograhic_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.cryptograhic_app.databinding.ActivityMainBinding
+import java.util.logging.Handler
+import android.os.Handler as Handler1
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // setting listners
+
+        }
     }
-    
-
-
-}
